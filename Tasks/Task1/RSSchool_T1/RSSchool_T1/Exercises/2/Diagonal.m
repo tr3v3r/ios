52f1f5a55dc9b67526c6c843fd88e65a621f1fa3
Aliaksei Astafyeu <lochost40@gmail.com>
2019-03-24 23:16:00 +0300

    Start second task

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/2/Diagonal.m
@@ -4,7 +4,21 @@
 
 // Complete the diagonalDifference function below.
 - (NSNumber *) diagonalDifference:(NSArray *)array {
+    double diagonalOne = 0;
+    double diagonalTwo = 0;
+    
+    NSString *rowString = array[0];
+    
+    NSArray * rowArray = [rowString componentsSeparatedByString:@" "];
+    
+    NSLog(@"%@", rowArray);
+    
+    int i;
+    int j;
+    
+    
     return 0;
+
 }
 
 @end
--- Tasks/Task1/RSSchool_T1/RSSchool_T1/main.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/main.m
@@ -1,8 +1,11 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "Diagonal.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        Diagonal *test = [[Diagonal alloc] init];
+        [test diagonalDifference: @[@"1 2 3"]];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
