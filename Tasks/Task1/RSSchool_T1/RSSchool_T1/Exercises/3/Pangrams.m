98c64562f920532c9fde2902c1e56d0f56a58109
Aliaksei Astafyeu <lochost40@gmail.com>
2019-03-25 22:00:57 +0300

    Finish fourth taskj

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,29 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    NSString *lower = [string lowercaseString];
+    
+    NSMutableSet *alphabet = [NSMutableSet new];
+    
+    unichar buffer[lower.length+1];
+    [lower getCharacters:buffer range:NSMakeRange(0, lower.length)];
+    
+    for(int i = 0; i < lower.length; i++) {
+        unichar ch = buffer[i];
+        BOOL test = ch >= (char)"a";
+        BOOL test2 = ch <= (unichar)"z";
+        if(test && test2) {
+            NSString *key = [NSString stringWithFormat:@"%c", buffer[i]];
+            [alphabet addObject: key];
+        }
+        
+    }
+
+    if([alphabet count] != 26) {
+        return NO;
+    }
+    
+    return YES;
 }
 
 @end
